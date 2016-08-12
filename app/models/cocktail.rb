6f69dac31b317f067f0f6c95e56760e18723a6f3
Ahmad Al Msouty <ahmad.msouti@Hotmail.com>
2016-08-12 14:46:38 +0300

    finished skeleton for cocktails the right way

---
+++ app/models/cocktail.rb
@@ -0,0 +1,6 @@
+class Cocktail < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
+  has_many :doses, dependent: :delete_all
+  has_many :ingredients, through: :doses
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+
+lemon = Ingredient.create(name: 'lemon')
+ice = Ingredient.create(name: 'ice')
+vodka = Ingredient.create(name: 'vodka')
+
+Cocktail.destroy_all
+martini = Cocktail.create(name: 'martini')
+
+Dose.destroy_all
+Dose.create(description: '1 oz. lemon juice', ingredient: lemon, cocktail: martini)
+Dose.create(description: '1/2 cup ice', ingredient: ice, cocktail: martini)
+Dose.create(description: '4 oz. vodka', ingredient: vodka, cocktail: martini)
+
+
